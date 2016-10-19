ea16634690bb389976147c769f2ac4ee54cc9615
Shige Takeda <smtakeda@gmail.com>
2016-10-18 17:20:01 -0700

    SNOW-24699: added VARIANT, OBJECT and ARRAY datatype support (#12)

--- __init__.py
+++ __init__.py
@@ -4,11 +4,11 @@
 # Copyright (c) 2012-2016 Snowflake Computing Inc. All right reserved.
 #
 
+from .base import (VARIANT, ARRAY, OBJECT)
+from .util import _url as URL
 from .version import VERSION
 from ..connector.compat import (TO_UNICODE)
 
 SNOWFLAKE_CONNECTOR_VERSION = '.'.join(TO_UNICODE(v) for v in VERSION[0:3])
 
 __version__ = SNOWFLAKE_CONNECTOR_VERSION
-
-from .util import _url as URL
