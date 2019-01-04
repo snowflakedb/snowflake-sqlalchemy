0db5f9c57f0228b858d209e118a086b6afcb4f94
Shige Takeda <smtakeda@gmail.com>
2019-01-04 15:23:59 +0000

    updated python 3.4 description

--- setup.py
+++ setup.py
@@ -70,6 +70,7 @@ setup(
 
         'Programming Language :: SQL',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
