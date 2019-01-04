31d380f05d4bdd833820852eb904843a8242b549
Shige Takeda <smtakeda@gmail.com>
2019-01-04 15:23:59 +0000

    updated python 3.4 description

--- scripts/install.sh
+++ scripts/install.sh
@@ -12,10 +12,12 @@ pip install -U virtualenv
 python -m virtualenv venv
 source ./venv/bin/activate
 if [[ "$TRAVIS_PYTHON_VERSION" == "3.4" ]]; then
+    # last pandas supporting Python 3.4
     pip install pandas==0.20.3
 else
     pip install pandas
 fi
+
 pip install pytest pytest-cov pytest-rerunfailures
 if [[ "$TRAVIS_PYTHON_VERSION" == "2.7" ]]; then
     pip install mock
