cac90f77a72194aca10b738c38918012b55ca4a2
Shige Takeda <smtakeda@gmail.com>
2019-01-04 05:49:44 +0000

    removed Python 3.4 test

--- setup.py
+++ setup.py
@@ -70,8 +70,9 @@ setup(
 
         'Programming Language :: SQL',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
 
         'Topic :: Database',
         'Topic :: Software Development',
