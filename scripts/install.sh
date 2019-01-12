468cfd3ce289efb13dd8c64c93ec3bce05920667
Shige Takeda <smtakeda@gmail.com>
2019-01-12 06:48:21 +0000

    Added Azure test

--- scripts/install.sh
+++ scripts/install.sh
@@ -3,13 +3,32 @@
 # Install Snowflake SQLAlchemy dialect
 #
 set -o pipefail
-sudo apt-get update
-openssl aes-256-cbc -k "$super_secret_password" -in parameters.py.enc -out test/parameters.py -d
-curl -O https://bootstrap.pypa.io/get-pip.py
-python get-pip.py
-pip --version
-pip install -U virtualenv
-python -m virtualenv venv
+
+if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+    brew update
+    brew install openssl readline sqlite3 xz zlib
+    brew outdated pyenv || brew upgrade pyenv
+    brew install pyenv-virtualenv
+    pyenv install ${PYTHON_VERSION}
+    export PYENV_VERSION=$PYTHON
+    export PATH="${HOME}/.pyenv/shims:${PATH}"
+    if [[ $PYTHON_VERSION == "2.7"* ]]; then
+        pip install -U virtualenv
+        python -m virtualenv venv
+    else
+        python3 -m venv venv
+    fi
+else
+    sudo apt-get update
+    pip install -U virtualenv
+    python -m virtualenv venv
+fi
+if [[ -n "$SNOWFLAKE_AZURE" ]]; then
+    openssl aes-256-cbc -k "$super_azure_secret_password" -in parameters_az.py.enc -out test/parameters.py -d
+else
+    openssl aes-256-cbc -k "$super_secret_password" -in parameters.py.enc -out test/parameters.py -d
+fi
+
 source ./venv/bin/activate
 if [[ "$TRAVIS_PYTHON_VERSION" == "3.4" ]]; then
     # last pandas supporting Python 3.4
