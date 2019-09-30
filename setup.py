98c1bcec5744ab6e9b64d5fbcf6bd9339110b69e
Shige Takeda <shige.takeda@snowflake.com>
2019-09-30 12:59:19 -0400

    SNOW-99934: Removed the required Python Connector version.

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
 
     install_requires = [
         'sqlalchemy<2.0.0',
-        'snowflake-connector-python<2.0.0',
+        'snowflake-connector-python',
     ],
     namespace_packages=[
         'snowflake'
