1ec623adf7cea55a834eadde3963bdd03d52c1fb
Mark Keller <mark.keller@snowflake.com>
2019-12-27 17:45:03 +0000

    possible codecov fix

--- scripts/run_travis.sh
+++ scripts/run_travis.sh
@@ -5,6 +5,6 @@
 set -o pipefail
 source ./venv/bin/activate
 ret=0
-timeout -s SIGUSR1 3600s py.test -vvv --cov=snowflake.sqlalchemy test || ret=$?
+timeout -s SIGUSR1 3600s py.test -vvv --cov=snowflake.sqlalchemy --cov-report=xml:sqlalchemy_${TRAVIS_PYTHON_VERSION}_coverage.xml test || ret=$?
 # TIMEOUT or SUCCESS
 [ $ret != 124 -a $ret != 0 ] && exit 1 || exit 0
