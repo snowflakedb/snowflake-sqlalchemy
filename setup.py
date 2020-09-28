196a2c23f9d352f9465b98932e271d0184211894
Mark Keller <63477823+sfc-gh-mkeller@users.noreply.github.com>
2020-09-28 17:26:19 +0000

    pin pytest to work with pytest-rerunfailures (#13972)

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
     ],
     extras_require={
         'development': [
-            'pytest',
+            'pytest<6.1.0',
             'pytest-cov',
             'pytest-rerunfailures',
             'pytest-timeout',
