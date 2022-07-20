2db5167b51cd511512cc941a976b27df185d0cb9
Adam Ling <adam.ling@snowflake.com>
2022-07-20 10:27:51 -0700

    SNOW-629074 unstable sqlalchemy tests due to missing table/sequence (#318)

--- tests/sqlalchemy_test_suite/conftest.py
+++ tests/sqlalchemy_test_suite/conftest.py
@@ -2,6 +2,8 @@
 # Copyright (c) 2012-2022 Snowflake Computing Inc. All rights reserved.
 #
 
+import sqlalchemy.testing.config
+from sqlalchemy import util
 from sqlalchemy.dialects import registry
 from sqlalchemy.testing.plugin.pytestplugin import *  # noqa
 from sqlalchemy.testing.plugin.pytestplugin import (
@@ -15,10 +17,31 @@ import snowflake.connector
 from snowflake.sqlalchemy import URL
 
 from ..conftest import get_db_parameters
+from ..util import random_string
 
 registry.register("snowflake", "snowflake.sqlalchemy", "dialect")
 registry.register("snowflake.snowflake", "snowflake.sqlalchemy", "dialect")
-sqlalchemy_test_schema = "TEST_SCHEMA"
+TEST_SCHEMA = f"test_schema_{random_string(5)}"
+TEST_SCHEMA_2 = f"{TEST_SCHEMA}_2"
+
+
+# patch sqlalchemy.testing.config.Confi.__init__ for schema name randomization
+# same schema name would result in conflict as we're running tests in parallel in the CI
+def config_patched__init__(self, db, db_opts, options, file_config):
+    self._set_name(db)
+    self.db = db
+    self.db_opts = db_opts
+    self.options = options
+    self.file_config = file_config
+    self.test_schema = TEST_SCHEMA
+    self.test_schema_2 = TEST_SCHEMA_2
+
+    self.is_async = db.dialect.is_async and not util.asbool(
+        db.url.query.get("async_fallback", False)
+    )
+
+
+sqlalchemy.testing.config.Config.__init__ = config_patched__init__
 
 
 def pytest_sessionstart(session):
@@ -27,7 +50,7 @@ def pytest_sessionstart(session):
     # schema name with 'TEST_SCHEMA' is required by some tests of the sqlalchemy test suite
     with snowflake.connector.connect(**db_parameters) as con:
         con.cursor().execute(f"CREATE SCHEMA IF NOT EXISTS {db_parameters['schema']}")
-        con.cursor().execute(f"CREATE SCHEMA IF NOT EXISTS {sqlalchemy_test_schema};")
+        con.cursor().execute(f"CREATE SCHEMA IF NOT EXISTS {TEST_SCHEMA};")
     _pytest_sessionstart(session)
 
 
@@ -35,5 +58,5 @@ def pytest_sessionfinish(session):
     db_parameters = get_db_parameters()
     with snowflake.connector.connect(**db_parameters) as con:
         con.cursor().execute(f"DROP SCHEMA IF EXISTS {db_parameters['schema']}")
-        con.cursor().execute(f"DROP SCHEMA IF EXISTS f{sqlalchemy_test_schema}")
+        con.cursor().execute(f"DROP SCHEMA IF EXISTS f{TEST_SCHEMA}")
     _pytest_sessionfinish(session)
