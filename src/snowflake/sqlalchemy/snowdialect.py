d238f03718826318e0385b4fef97001ef49e58c9
Ken Terada <49501704+sfc-gh-kterada@users.noreply.github.com>
2022-11-12 14:47:33 -0800

    Update snowdialect.py
    Refactored...

--- src/snowflake/sqlalchemy/snowdialect.py
+++ src/snowflake/sqlalchemy/snowdialect.py
@@ -319,19 +319,12 @@ class SnowflakeDialect(default.DefaultDialect):
 
     @reflection.cache
     def _get_table_primary_keys(self, connection, schema, table_name, **kw):
-        if table_name in SnowflakeIdentifierPreparer.reserved_words:
-            fully_qualified_path = self._denormalize_quote_join(schema, table_name)
-            result = connection.execute(
-                text(
-                    f"SHOW /* sqlalchemy:_get_table_primary_keys */ PRIMARY KEYS IN TABLE {fully_qualified_path}"
-                )
+        fully_qualified_path = self._denormalize_quote_join(schema, self.denormalize_name(table_name))
+        result = connection.execute(
+            text(
+                f"SHOW /* sqlalchemy:_get_table_primary_keys */ PRIMARY KEYS IN TABLE {fully_qualified_path}"
             )
-        else:
-            result = connection.execute(
-                text(
-                    f"SHOW /* sqlalchemy:_get_table_primary_keys */ PRIMARY KEYS IN TABLE {schema}.{table_name}"
-                )
-            )
+        )
         ans = {}
         for row in result:
             table_name = self.normalize_name(row._mapping["table_name"])
@@ -388,19 +381,12 @@ class SnowflakeDialect(default.DefaultDialect):
 
     @reflection.cache
     def _get_table_unique_constraints(self, connection, schema, table_name, **kw):
-        if table_name in SnowflakeIdentifierPreparer.reserved_words:
-            fully_qualified_path = self._denormalize_quote_join(schema, table_name)
-            result = connection.execute(
-                text(
-                    f"SHOW /* sqlalchemy:_get_table_unique_constraints */ UNIQUE KEYS IN TABLE {fully_qualified_path}"
-                )
+        fully_qualified_path = self._denormalize_quote_join(schema, table_name)
+        result = connection.execute(
+            text(
+                f"SHOW /* sqlalchemy:_get_table_unique_constraints */ UNIQUE KEYS IN TABLE {fully_qualified_path}"
             )
-        else:
-            result = connection.execute(
-                text(
-                    f"SHOW /* sqlalchemy:_get_table_unique_constraints */ UNIQUE KEYS IN TABLE {schema}.{table_name}"
-                )
-            )
+        )
         unique_constraints = {}
         for row in result:
             name = self.normalize_name(row._mapping["constraint_name"])
@@ -471,19 +457,12 @@ class SnowflakeDialect(default.DefaultDialect):
     @reflection.cache
     def _get_table_foreign_keys(self, connection, schema, table_name, **kw):
         _, current_schema = self._current_database_schema(connection, **kw)
-        if table_name in SnowflakeIdentifierPreparer.reserved_words:
-            fully_qualified_path = self._denormalize_quote_join(schema, table_name)
-            result = connection.execute(
-                text(
-                    f"SHOW /* sqlalchemy:_get_table_foreign_keys */ IMPORTED KEYS IN TABLE {fully_qualified_path}"
-                )
+        fully_qualified_path = self._denormalize_quote_join(schema, self.denormalize_name(table_name))
+        result = connection.execute(
+            text(
+                f"SHOW /* sqlalchemy:_get_table_foreign_keys */ IMPORTED KEYS IN TABLE {fully_qualified_path}"
             )
-        else:
-            result = connection.execute(
-                text(
-                    f"SHOW /* sqlalchemy:_get_table_foreign_keys */ IMPORTED KEYS IN TABLE {schema}.{table_name}"
-                )
-            )
+        )
         foreign_key_map = {}
         for row in result:
             name = self.normalize_name(row._mapping["fk_name"])
