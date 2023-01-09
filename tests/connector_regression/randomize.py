41a06e27c33613d9362134946d83caa9292f92b5
Adam Ling <adam.ling@snowflake.com>
2023-01-09 09:28:00 -0800

    update license

--- tests/connector_regression/randomize.py
+++ tests/connector_regression/randomize.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
+# Copyright (c) 2012-2022 Snowflake Computing Inc. All rights reserved.
 #
 """
 This module was added back to the repository for compatibility with the old driver tests that
