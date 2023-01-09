b46f7d9487bf97d477d632960416a0532adbef1a
Adam Ling <adam.ling@snowflake.com>
2023-01-09 11:01:20 -0800

    add version file

--- setup.py
+++ setup.py
@@ -2,6 +2,16 @@
 # Copyright (c) 2012-2022 Snowflake Computing Inc. All rights reserved.
 #
 
+import os
+
 from setuptools import setup
 
-setup()
+SQLALCHEMY_SRC_DIR = os.path.join("src", "snowflake", "sqlalchemy")
+VERSION = (1, 1, 1, None)  # Default
+with open(os.path.join(SQLALCHEMY_SRC_DIR, "version.py"), encoding="utf-8") as f:
+    exec(f.read())
+    version = ".".join([str(v) for v in VERSION if v is not None])
+
+setup(
+    version=version,
+)
