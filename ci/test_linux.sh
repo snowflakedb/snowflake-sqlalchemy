2d2382605d0aa116bf49d66236cb863fb5fb77e6
Adam Ling <adam.ling@snowflake.com>
2023-02-08 16:35:24 -0800

    use tox4 feature

--- ci/test_linux.sh
+++ ci/test_linux.sh
@@ -2,7 +2,7 @@
 #
 # Test Snowflake SQLAlchemy in Linux
 # NOTES:
-#   - Versions to be tested should be passed in as the first argument, e.g: "3.7 3.8". If omitted 3.7-3.10 will be assumed.
+#   - Versions to be tested should be passed in as the first argument, e.g: "3.7 3.8". If omitted 3.7-3.11 will be assumed.
 #   - This script assumes that ../dist/repaired_wheels has the wheel(s) built for all versions to be tested
 #   - This is the script that test_docker.sh runs inside of the docker container
 
@@ -11,8 +11,7 @@ THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 SQLALCHEMY_DIR="$( dirname "${THIS_DIR}")"
 
 # Install one copy of tox
-python3 -m pip install -U tox-external-wheels
-python3 -m pip install --pre tox
+python3 -m pip install -U tox
 
 # Run tests
 cd $SQLALCHEMY_DIR
@@ -22,5 +21,5 @@ for PYTHON_VERSION in ${PYTHON_VERSIONS}; do
     SQLALCHEMY_WHL=$(ls $SQLALCHEMY_DIR/dist/snowflake_sqlalchemy-*-py2.py3-none-any.whl | sort -r | head -n 1)
     TEST_ENVLIST=fix_lint,py${SHORT_VERSION}-ci,py${SHORT_VERSION}-coverage
     echo "[Info] Running tox for ${TEST_ENVLIST}"
-    python3 -m tox -e ${TEST_ENVLIST} --external_wheels ${SQLALCHEMY_WHL}
+    python3 -m tox -e ${TEST_ENVLIST} --installpkg ${SQLALCHEMY_WHL}
 done
