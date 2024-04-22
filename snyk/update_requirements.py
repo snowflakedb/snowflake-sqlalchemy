76263c83d1110a63b1064f3d6fc3257e95d8631c
Marcin Raba <marcin.raba@snowflake.com>
2024-04-22 13:50:15 +0200

    mraba/snyk-support-requirements: add script for generating requirements and filename fix

---
+++ snyk/update_requirements.py
@@ -0,0 +1,16 @@
+from pathlib import Path
+
+import tomlkit
+
+
+def sync():
+    pyproject = tomlkit.loads(Path("pyproject.toml").read_text())
+    snyk_reqiurements = Path("snyk/requirements.txt")
+    dependencies = pyproject.get("project", {}).get("dependencies", [])
+
+    with snyk_reqiurements.open("w") as fh:
+        fh.write("\n".join(dependencies))
+
+
+if __name__ == "__main__":
+    sync()
