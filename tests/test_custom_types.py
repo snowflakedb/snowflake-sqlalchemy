1f8c4986eef564a18fb02fe53b031a266414f53e
Jorge Vasquez Rojas <jorge.vasquezrojas@snowflake.com>
2024-08-14 08:34:05 -0600

    Add test with large object

--- tests/test_custom_types.py
+++ tests/test_custom_types.py
@@ -2,7 +2,11 @@
 # Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
 #
 
-from snowflake.sqlalchemy import custom_types
+
+import pytest
+from sqlalchemy import Column, Integer, MetaData, Table
+
+from snowflake.sqlalchemy import TEXT, custom_types
 
 
 def test_string_conversions():
@@ -34,3 +38,20 @@ def test_string_conversions():
         sample = getattr(custom_types, type_)()
         if type_ in sf_custom_types:
             assert type_ == str(sample)
+
+
+@pytest.mark.feature_max_lob_size
+def test_create_table_with_text_type_and_max_lob_size(engine_testaccount):
+    metadata = MetaData()
+    table_name = "test_max_lob_size_0"
+    test_max_lob_size = Table(
+        table_name,
+        metadata,
+        Column("id", Integer, primary_key=True),
+        Column("name", TEXT(20000000), primary_key=True),
+    )
+    metadata.create_all(engine_testaccount)
+    try:
+        assert test_max_lob_size is not None
+    finally:
+        test_max_lob_size.drop(engine_testaccount)
