9073b468b2ab2a5e1b81c234fb7d29b15432eae8
Jan Cieślak <jan.cieslak@snowflake.com>
2025-11-25 09:12:11 +0100

    Small adjustments

--- tests/test_dialect_connect.py
+++ tests/test_dialect_connect.py
@@ -1,7 +1,6 @@
 #
 # Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
-
-"""Tests for telemetry behavior during SnowflakeDialect.connect()."""
+#
 
 from types import SimpleNamespace
 from unittest.mock import MagicMock
