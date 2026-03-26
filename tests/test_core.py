25ab75140647299dd873ec83fd1dff6f0ddf7c2c
Jan Cieslak <jan.cieslak@snowflake.com>
2026-03-26 07:55:40 +0000

    Fix core test

--- tests/test_core.py
+++ tests/test_core.py
@@ -780,7 +780,7 @@ def test_naming_convention_constraint_names(engine_testaccount):
         users.drop(engine_testaccount)
 
 
-def test_get_multile_column_primary_key(engine_testaccount):
+def test_get_multiple_column_primary_key(engine_testaccount):
     """
     Tests multicolumn primary key with and without autoincrement
     """
@@ -806,7 +806,9 @@ def test_get_multile_column_primary_key(engine_testaccount):
         assert columns_in_mytable[1]["primary_key"], "primary key"
 
         primary_keys = inspector.get_pk_constraint("mytable")
-        assert primary_keys["constrained_columns"] == ["gid", "id"]
+
+        # Different from column order, as it seems SQLAlchemy produces the following constraint: PRIMARY KEY (id, gid)
+        assert primary_keys["constrained_columns"] == ["id", "gid"]
 
     finally:
         mytable.drop(engine_testaccount)
