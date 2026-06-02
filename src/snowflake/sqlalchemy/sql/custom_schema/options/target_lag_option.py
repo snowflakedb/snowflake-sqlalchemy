c52338ceedee709ced36766d69f2b6fb0ca5a8b9
Marcin Rafal Raba <marcin.raba@snowflake.com>
2026-06-02 09:48:00 +0000

    fix code annotations for TargetLagOption

--- src/snowflake/sqlalchemy/sql/custom_schema/options/target_lag_option.py
+++ src/snowflake/sqlalchemy/sql/custom_schema/options/target_lag_option.py
@@ -1,8 +1,9 @@
 #
 # Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
-# from enum import Enum
+from __future__ import annotations
+
 from enum import Enum
-from typing import Optional, Tuple, Union
+from typing import Any
 
 from snowflake.sqlalchemy.custom_commands import NoneType
 
@@ -42,8 +43,8 @@ class TargetLagOption(TableOption):
 
     def __init__(
         self,
-        time: Optional[int] = 0,
-        unit: Optional[TimeUnit] = TimeUnit.MINUTES,
+        time: int | None = 0,
+        unit: TimeUnit | None = TimeUnit.MINUTES,
     ) -> None:
         super().__init__()
         self.time = time
@@ -52,12 +53,12 @@ class TargetLagOption(TableOption):
 
     @staticmethod
     def create(
-        value: Union["TargetLagOption", Tuple[int, TimeUnit], KeywordOptionType],
-    ) -> Optional[TableOption]:
+        value: TargetLagOption | tuple[int, TimeUnit] | KeywordOptionType,
+    ) -> TableOption | None:
         if isinstance(value, NoneType):
             return value
 
-        if isinstance(value, Tuple):
+        if isinstance(value, tuple):
             time, unit = value
             value = TargetLagOption(time, unit)
 
@@ -72,23 +73,23 @@ class TargetLagOption(TableOption):
             str(type(value).__name__),
             [
                 TargetLagOption.__name__,
-                f"Tuple[int, {TimeUnit.__name__}])",
+                f"tuple[int, {TimeUnit.__name__}]",
                 SnowflakeKeyword.__name__,
             ],
         )
 
-    def __get_expression(self):
+    def __get_expression(self) -> str:
         return f"'{str(self.time)} {str(self.unit.value)}'"
 
     @property
     def priority(self) -> Priority:
         return Priority.HIGH
 
-    def _render(self, compiler) -> str:
+    def _render(self, compiler: Any) -> str:
         return self.template() % (self.__get_expression())
 
     def __repr__(self) -> str:
         return "TargetLagOption(%s)" % self.__get_expression()
 
 
-TargetLagOptionType = Union[TargetLagOption, Tuple[int, TimeUnit]]
+TargetLagOptionType = TargetLagOption | tuple[int, TimeUnit]
